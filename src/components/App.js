72832b2e0d739109ebe63175c96bce739203276d
Kevin Harrison <kharrison7@elon.edu>
2017-09-13 14:23:28 -0400

    This adds live earthquakes from the hard mode.

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import moment from 'moment';
 // import logo from '../logo.svg';
 import EarthquakeInfo from './EarthquakeInfo.js';
 import EarthquakeList from './EarthquakeList.js';
+import EarthquakeListLive from './LiveQuakes.js';
+
 
 import '../styles/App.css';
 
@@ -11,8 +13,16 @@ class App extends Component {
     return (
       <div className="App">
       <div className="title">
+      <div className="my-header">
+      "Live Earthquakes!"
+      </div>
+        <EarthquakeListLive />
+        <br />
+        <hr />
+            <div className="title">
             <div className="my-header">
-            "Earthquakes!"
+            "Example Earthquakes!"
+            </div>
             </div>
             <EarthquakeInfo />
             <EarthquakeList />
---
+++ src/components/LiveQuakes.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import earthquakes from '../data/earthquakes.js';
+import axios from 'axios';
+
+export default class EarthquakeListLive extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      listofLivequakes: []
+    };
+  }
+  componentDidMount() {
+    axios.get(`https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson`)
+      .then(res => {
+        const listofLivequakes = res.data.features;
+        this.setState({ listofLivequakes });
+      });
+  }
+  render() {
+    return (
+      <div className="quake-list">
+        <div className="row">
+        {this.state.listofLivequakes.map(quake =>
+          <div className="col-sm-6" key={quake.id}>
+            <div className="card" >
+              <div className="card-block">
+                <h4 className="card-title">{quake.properties.place}</h4>
+                <h6 className="card-subtitle mb-2 text-muted">Magnitude: {quake.properties.mag}</h6>
+                <h6 className="card-subtitle mb-2 text-muted">Time: {moment(quake.properties.time).format('llll')}</h6>
+                <p className="card-text">Coordinates: {quake.geometry.coordinates}</p>
+                <a href={quake.properties.url} className="card-link">USGS Event Link</a>
+              </div>
+            </div>
+          </div>
+         )}
+        </div>
+      </div>
+    );
+  }
+}
